6e8083792514b517e1262d73a02bc5c85cfebcc3
kai4711x <zellview2@gmail.com>
2019-12-06 14:04:49 +0100

    refinig install-wine.sh, no requests anymore

--- Script/install-wine.sh
+++ Script/install-wine.sh
@@ -1,10 +1,10 @@
 
 #Wine
-sudo dpkg --add-architecture i386
+sudo dpkg --add-architecture i386 
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
-sudo apt update
-sudo apt install --install-recommends winehq-stable
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main' -y
+sudo apt update -y
+sudo apt install --install-recommends winehq-stable -y
 
 
