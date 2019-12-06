1c0f6a0147b0958853a55a2dd258a9dc0fbcba23
kai4711x <zellview2@gmail.com>
2019-12-06 14:38:10 +0100

    update install-virtualbox.sh to V5, no requests anymore

--- Script/install-virtualBox.sh
+++ Script/install-virtualBox.sh
@@ -1,8 +1,8 @@
 
 #   VirtualBox
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
@@ -10,8 +10,8 @@ wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key a
 sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian xenial contrib"
 
 
-sudo apt-get update
-sudo apt-get install virtualbox-5.2
+sudo apt-get update -y
+sudo apt-get install virtualbox-6.0
 
 
 # Virtual Disk to ISO
